b6ec1ad719b87cd66fd6d9c6630a84ff4106526e
abrahamrhoffman <abrahamrhoffman@gmail.com>
2019-03-18 09:29:05 -0700

    test

---
+++ prime_finder/target/debug/deps/prime_finder-097c1f6a037236c0.d
@@ -0,0 +1,5 @@
+/Users/abhoffman/Dropbox/abe/hoffman-dynamics/math/prime_finder/target/debug/deps/prime_finder-097c1f6a037236c0: src/main.rs
+
+/Users/abhoffman/Dropbox/abe/hoffman-dynamics/math/prime_finder/target/debug/deps/prime_finder-097c1f6a037236c0.d: src/main.rs
+
+src/main.rs:
---
+++ prime_finder/target/debug/prime_finder.d
@@ -0,0 +1,1 @@
+/Users/abhoffman/Dropbox/abe/hoffman-dynamics/math/prime_finder/target/debug/prime_finder.dSYM: /Users/abhoffman/Dropbox/abe/hoffman-dynamics/math/prime_finder/src/main.rs
